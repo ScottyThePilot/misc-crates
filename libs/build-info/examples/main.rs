85a3635260e824075deecb16aebabe81b51b5dca
ScottyThePilot <scotty.codes@gmail.com>
2025-10-16 02:31:16 -0400

    `build-info`: Fix example compile error

--- libs/build-info/examples/main.rs
+++ libs/build-info/examples/main.rs
@@ -1,27 +1,35 @@
 extern crate build_info;
-extern crate chrono;
 
-use chrono::{DateTime, Utc, FixedOffset};
+#[cfg(feature = "git")]
+mod git_example {
+  pub const GIT_HASH: &str = build_info::git_hash!();
+  pub const GIT_HASH_SHORT: &str = build_info::git_hash_short!();
+  pub const GIT_BRANCH: &str = build_info::git_branch!();
+  pub const GIT_ROOT: &str = build_info::git_root!();
+}
 
-pub const GIT_HASH: &str = build_info::git_hash!();
-pub const GIT_HASH_SHORT: &str = build_info::git_hash_short!();
-pub const GIT_BRANCH: &str = build_info::git_branch!();
-pub const GIT_ROOT: &str = build_info::git_root!();
-pub const BUILD_DATETIME_UTC: DateTime<Utc> = build_info::build_datetime_utc!();
-pub const BUILD_DATETIME_UTC_FORMAT: &str = build_info::build_datetime_utc_format!("%d/%m/%Y %H:%M:%S %Z");
-pub const BUILD_DATETIME_UTC_RFC2822: &str = build_info::build_datetime_utc_rfc2822!();
-pub const BUILD_DATETIME_UTC_RFC3339: &str = build_info::build_datetime_utc_rfc3339!();
-pub const BUILD_DATETIME_LOCAL_FIXED: DateTime<FixedOffset> = build_info::build_datetime_local_fixed!();
-pub const BUILD_DATETIME_LOCAL_FIXED_FORMAT: &str = build_info::build_datetime_local_fixed_format!("%d/%m/%Y %H:%M:%S %Z");
-pub const BUILD_DATETIME_LOCAL_FIXED_RFC2822: &str = build_info::build_datetime_local_fixed_rfc2822!();
-pub const BUILD_DATETIME_LOCAL_FIXED_RFC3339: &str = build_info::build_datetime_local_fixed_rfc3339!();
-pub const GIT_LAST_COMMIT_DATETIME_UTC: DateTime<Utc> = build_info::git_last_commit_datetime_utc!();
-pub const GIT_LAST_COMMIT_DATETIME_UTC_FORMAT: &str = build_info::git_last_commit_datetime_utc_format!("%d/%m/%Y %H:%M:%S %Z");
-pub const GIT_LAST_COMMIT_DATETIME_UTC_RFC2822: &str = build_info::git_last_commit_datetime_utc_rfc2822!();
-pub const GIT_LAST_COMMIT_DATETIME_UTC_RFC3339: &str = build_info::git_last_commit_datetime_utc_rfc3339!();
-pub const GIT_LAST_COMMIT_DATETIME_LOCAL_FIXED: DateTime<FixedOffset> = build_info::git_last_commit_datetime_local_fixed!();
-pub const GIT_LAST_COMMIT_DATETIME_LOCAL_FIXED_FORMAT: &str = build_info::git_last_commit_datetime_local_fixed_format!("%d/%m/%Y %H:%M:%S %Z");
-pub const GIT_LAST_COMMIT_DATETIME_LOCAL_FIXED_RFC2822: &str = build_info::git_last_commit_datetime_local_fixed_rfc2822!();
-pub const GIT_LAST_COMMIT_DATETIME_LOCAL_FIXED_RFC3339: &str = build_info::git_last_commit_datetime_local_fixed_rfc3339!();
+#[cfg(feature = "chrono")]
+mod chrono_example {
+  extern crate chrono;
+
+  use chrono::{DateTime, Utc, FixedOffset};
+
+  pub const BUILD_DATETIME_UTC: DateTime<Utc> = build_info::build_datetime_utc!();
+  pub const BUILD_DATETIME_UTC_FORMAT: &str = build_info::build_datetime_utc_format!("%d/%m/%Y %H:%M:%S %Z");
+  pub const BUILD_DATETIME_UTC_RFC2822: &str = build_info::build_datetime_utc_rfc2822!();
+  pub const BUILD_DATETIME_UTC_RFC3339: &str = build_info::build_datetime_utc_rfc3339!();
+  pub const BUILD_DATETIME_LOCAL_FIXED: DateTime<FixedOffset> = build_info::build_datetime_local_fixed!();
+  pub const BUILD_DATETIME_LOCAL_FIXED_FORMAT: &str = build_info::build_datetime_local_fixed_format!("%d/%m/%Y %H:%M:%S %Z");
+  pub const BUILD_DATETIME_LOCAL_FIXED_RFC2822: &str = build_info::build_datetime_local_fixed_rfc2822!();
+  pub const BUILD_DATETIME_LOCAL_FIXED_RFC3339: &str = build_info::build_datetime_local_fixed_rfc3339!();
+  pub const GIT_LAST_COMMIT_DATETIME_UTC: DateTime<Utc> = build_info::git_last_commit_datetime_utc!();
+  pub const GIT_LAST_COMMIT_DATETIME_UTC_FORMAT: &str = build_info::git_last_commit_datetime_utc_format!("%d/%m/%Y %H:%M:%S %Z");
+  pub const GIT_LAST_COMMIT_DATETIME_UTC_RFC2822: &str = build_info::git_last_commit_datetime_utc_rfc2822!();
+  pub const GIT_LAST_COMMIT_DATETIME_UTC_RFC3339: &str = build_info::git_last_commit_datetime_utc_rfc3339!();
+  pub const GIT_LAST_COMMIT_DATETIME_LOCAL_FIXED: DateTime<FixedOffset> = build_info::git_last_commit_datetime_local_fixed!();
+  pub const GIT_LAST_COMMIT_DATETIME_LOCAL_FIXED_FORMAT: &str = build_info::git_last_commit_datetime_local_fixed_format!("%d/%m/%Y %H:%M:%S %Z");
+  pub const GIT_LAST_COMMIT_DATETIME_LOCAL_FIXED_RFC2822: &str = build_info::git_last_commit_datetime_local_fixed_rfc2822!();
+  pub const GIT_LAST_COMMIT_DATETIME_LOCAL_FIXED_RFC3339: &str = build_info::git_last_commit_datetime_local_fixed_rfc3339!();
+}
 
 fn main() {}
